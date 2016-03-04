41f5f7fe50a531a892b06c34182668fb906ba4f9
Jacob Lustig-Yaeger <jlustigy@uw.edu>
2016-03-03 23:04:18 -0800

    Now with more cowbell!

--- toolbox.py
+++ toolbox.py
@@ -9,16 +9,6 @@ import numpy as np
 
 def find_nearest(array,value):
     """Finds index of array nearest to the value
-    
-    Parameters
-    ----------
-    array : array
-    value : float
-    
-    Returns
-    -------
-    idx : int
-        Index in array nearest value
     """
     idx = (np.abs(array-value)).argmin()
     return idx
